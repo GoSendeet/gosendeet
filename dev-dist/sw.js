96fd1ea7efc512b728f0eb214f4222d102ddc46c
code-lova <ebinuga@hotmail.com>
2026-05-25 04:58:17 +0100

    fix: make validation error visible to user on mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2lpml31uo5k"
+    "revision": "0.qhbt3tvnjn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
