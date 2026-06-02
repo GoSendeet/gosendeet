c07e84b6932f942fd4bb3769eb2da9e12d10d575
tbello <bello4aus@gmail.com>
2026-06-02 04:21:03 +0100

    refactor: clean up service worker code formatting and update index.html revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qhbt3tvnjn8"
+    "revision": "0.mrtqe7g69b4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
