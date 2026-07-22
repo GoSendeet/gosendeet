252ea2b875a83f7f865629ffc365f851f0fbd0fd
Emmanuel Ebenezer <ekefan.dev@gmail.com>
2026-07-22 10:28:13 +0100

    redirect user back to the normal chat

--- src/pages/home/CostCalculator/components/Calculator/Booking/whatsappUtil.ts
+++ src/pages/home/CostCalculator/components/Calculator/Booking/whatsappUtil.ts
@@ -1,16 +1,19 @@
- export const formatWhatsAppNumber = (rawNumber: string) => {
-    const digitsOnly = rawNumber.replace(/\D/g, "");
-    if (digitsOnly.startsWith("234")) {
-      return digitsOnly;
-    }
-    if (digitsOnly.startsWith("0")) {
-      return `234${digitsOnly.slice(1)}`;
-    }
-    return `234${digitsOnly}`;
-  };
+export const formatWhatsAppNumber = (rawNumber: string) => {
+  const digitsOnly = rawNumber.replace(/\D/g, "");
+  if (digitsOnly.startsWith("234")) {
+    return digitsOnly;
+  }
+  if (digitsOnly.startsWith("0")) {
+    return `234${digitsOnly.slice(1)}`;
+  }
+  return `234${digitsOnly}`;
+};
 
-export  const handleBackToWhatsapp = (userWhatsappNumber: string) => {
-    if (!userWhatsappNumber) return;
-    const formattedNumber = formatWhatsAppNumber(userWhatsappNumber);
-    window.location.href = `https://wa.me/${formattedNumber}`;
-  };
+export const handleBackToWhatsapp = (userWhatsappNumber: string) => {
+  if (!userWhatsappNumber) return;
+  const formattedNumber = formatWhatsAppNumber(userWhatsappNumber);
+  console.log(formatWhatsAppNumber)
+  const defaultTestNumber = `15551523182`
+  // TODO: update defaultTestNumber, pull whatsapp number from env and remove whatsapp number from the urls
+  window.location.href = `https://wa.me/${defaultTestNumber}`;
+};
